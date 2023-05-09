16300850ea03d00ab8031676814fe3ee7ba087ad
Tisna Hadiana <tisnahadiana@gmail.com>
2023-05-09 09:30:31 +0700

    Adding OrderButton Component

---
+++ app/src/main/java/id/tisnahadiana/jetpadang/ui/components/OrderButton.kt
@@ -0,0 +1,42 @@
+package id.tisnahadiana.jetpadang.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import id.tisnahadiana.jetpadang.ui.theme.JetPadangTheme
+
+@Composable
+fun OrderButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        enabled = enabled,
+        modifier = modifier.fillMaxWidth().height(52.dp)
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun OrderButtonPreview() {
+    JetPadangTheme() {
+        OrderButton(
+            text = "Order",
+            onClick = {}
+        )
+    }
+}
